46dc8650cf283d8326f5e11f62b50afd6047dc00
GeorgmanyRuiz <37196080+GeorgmanyRuiz@users.noreply.github.com>
2021-10-20 04:17:33 -0400

    My Portfolio

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+ fields.fullName = document.getElementById('fullName');
+ fields.email = document.getElementById('email');
+ fields.textarea = document.getElementById('question');
+})
